148b649d42a35e47bc9de4167f77db0e88c5df92
Bernardo Gonçalves Spinelli Martins <bernardospinelli@hotmail.com.br>
2025-04-30 18:29:37 -0300

    feat: Adição aula 98 e 99

---
+++ aulas/aula99_package/__init__.py
@@ -0,0 +1,11 @@
+# print(
+#     'Você importou o ', __name__
+# )
+
+# def dobra(x):
+#     return x * 2
+
+# from aula99_package.modulo import nova_variavel, variavel, soma_do_modulo
+
+from aula99_package.modulo import *
+from aula99_package.modulo_b import *
---
+++ aulas/aula99_package/modulo.py
@@ -0,0 +1,17 @@
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+#     'nova_variavel',
+
+# ]
+
+# from aula99_package.modulo_b import fala_oi
+
+variavel = 'Alguma coisa'
+
+def soma_do_modulo(a, b):
+    return a + b
+
+nova_variavel = 'Outra coisa'
+
+# fala_oi()
