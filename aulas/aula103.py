f6629bd4222336de1335cadb3962e0b430b7224c
Bernardo Gonçalves Spinelli Martins <bernardospinelli@hotmail.com.br>
2025-05-01 15:48:14 -0300

    feat: Exercício 103 1

--- aulas/aula103.py
+++ aulas/aula103.py
@@ -3,6 +3,7 @@
 # Funções decoradoras são funções que decoram outras funções
 # Decoradores são usados para fazer o Python
 # usar as funções decoradoras em outras funções.
+# Decoradores são "Syntax Sugar" (Açúcar sintático)
 
 def criar_funcao(func):
     def interna(*args, **kwargs):
@@ -16,7 +17,9 @@ def criar_funcao(func):
         return resultado
     return interna
 
+@criar_funcao
 def inverte_string(string):
+    print(f'{inverte_string.__name__} foi chamada')
     return string[::-1]
 
 def e_string(param):
@@ -24,6 +27,5 @@ def e_string(param):
         raise TypeError('O parâmetro deve ser uma string')
     return True
 
-inverte_string_checando_parametro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_parametro('123')
+invertida = inverte_string('123')
 print(invertida)
