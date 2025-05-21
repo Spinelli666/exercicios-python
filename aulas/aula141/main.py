5f3aa7e425dca9a965cb635ab82e687d5309a8d9
Bernardo Gonçalves Spinelli Martins <bernardospinelli@hotmail.com.br>
2025-05-21 14:35:48 -0300

    feat: Aula 138 até 147

---
+++ aulas/aula141/main.py
@@ -0,0 +1,9 @@
+# from log import LogPrintMixin, LogFileMixin
+
+from eletronico import Smartphone
+
+galaxy_s20 = Smartphone("Galaxy S")
+iphone = Smartphone("iPhone")
+
+galaxy_s20.ligar()
+iphone.desligar()
